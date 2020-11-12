861d0e7e8e6254eda473340447f890c1b6d5ddc5
Latiful Mousom <Latifulmousom@gmail.com>
2020-11-12 20:50:13 +0530

    js-masterclass

---
+++ app.js
@@ -0,0 +1,15 @@
+var amarta = document.getElementById("amarta");
+var banasri = document.getElementById("banasri");
+var braja = document.getElementById("braja");
+var deb = document.getElementById("deb");
+var laxmi = document.getElementById("laxmi");
+var mani = document.getElementById("mani");
+var pritam = document.getElementById("pritam");
+
+amarta.innerHTML = 10;
+//banasri.innerHTML = 00;
+//braja.innerHTML = 00;
+//deb.innerHTML = 00;
+//laxmi.innerHTML = 00;
+mani.innerHTML = 10;
+//pritam.innerHTML = 00;
