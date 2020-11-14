8ab2d267f27296897e7670a8091068c0d05f940f
Latiful Mousom <Latifulmousom@gmail.com>
2020-11-14 10:34:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-var amarta = document.getElementById("amarta");
-var banasri = document.getElementById("banasri");
-var braja = document.getElementById("braja");
-var deb = document.getElementById("deb");
-var laxmi = document.getElementById("laxmi");
-var mani = document.getElementById("mani");
-var pritam = document.getElementById("pritam");
+var amarta = document.querySelector('#amarta');
+var banasri = document.querySelector('#banasri');
+var braja = document.querySelector('#braja');
+var deb = document.querySelector('#deb');
+var laxmi = document.querySelector('#laxmi');
+var mani = document.querySelector('#mani');
+var pritam = document.querySelector('#pritam');
 
-amarta.innerHTML = 10;
-banasri.innerHTML = 10;
-//braja.innerHTML = 00;
-//deb.innerHTML = 00;
-//laxmi.innerHTML = 00;
-mani.innerHTML = 10;
-pritam.innerHTML = 10;
+function renderCafe(doc){
+    const amarta = document.querySelector('#amarta');
+    amarta.textContent = doc('Jl9EFQrxIhCxQymkCVXt').data().marks;
+   
+
+    
+
+    // updating data
+   
+}
+
+// real-time listener
+db.collection('student-data').orderBy('marks').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        console.log(change.doc.data());
+        if(change.type == 'added'){
+            renderCafe(change.doc);
+        } else if (change.type == 'removed'){
+            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+            cafeList.removeChild(li);
+        }
+    });
+});
