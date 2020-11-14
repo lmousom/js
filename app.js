c744460900fdd25e73d56a2707da730ae402123b
Latiful Mousom <Latifulmousom@gmail.com>
2020-11-14 11:23:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-function renderCafe(doc){
-    const amarta = document.querySelector('#amarta');
+const amarta = document.querySelector('#amarta');
 const banasri = document.querySelector('#banasri');
 const braja = document.querySelector('#braja');
 const deb = document.querySelector('#deb');
 const laxmi = document.querySelector('#laxmi');
 const mani = document.querySelector('#mani');
 const pritam = document.querySelector('#pritam');
-    
-    amarta.textContent = doc().data().marks;
-   
 
-    
 
-    // updating data
-   
-}
-db.collection('student-data').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        renderCafe(doc);
-    });
+var ref = firebase.database().ref();
+
+ref.on("value", function(snapshot) {
+   console.log(snapshot.val());
+}, function (error) {
+   console.log("Error: " + error.code);
 });
-
