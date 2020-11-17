1b7c6d841daf41fa886e4f93958c3289e50b2fa1
Latiful Mousom <Latifulmousom@gmail.com>
2020-11-17 22:07:15 +0530

    student delete

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const amarta = document.querySelector('#amarta');
 const banasri = document.querySelector('#banasri');
-const braja = document.querySelector('#braja');
 const deb = document.querySelector('#deb');
 const laxmi = document.querySelector('#laxmi');
 const mani = document.querySelector('#mani');
@@ -36,16 +35,6 @@ studentsRef2.get().then(function(doc) {
 
 var studentsRef3 = db.collection("student-data").doc("EZYQNyYX0YTaJH0lXjqf");
 
-studentsRef3.get().then(function(doc) {
-    if (doc.exists) {
-        braja.textContent = doc.data().marks;
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-}).catch(function(error) {
-    console.log("Error getting document:", error);
-});
 
 
 var studentsRef4 = db.collection("student-data").doc("Yk03trXAk1WkBFJXQN45");
