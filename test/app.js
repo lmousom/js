b2a6db3d77933a3d0da27b5cf81069cafaf0e835
Latiful Mousom <Latifulmousom@gmail.com>
2020-11-21 11:44:54 +0530

    Add files via upload

---
+++ test/app.js
@@ -0,0 +1,40 @@
+const DB = 2001;
+const DB2 = 2002;
+const x = 2050;
+// 2050
+const age = x - DB;
+const age2 = x - DB2;
+console.log(age, age2);
+
+
+const name = 'Moni';
+const moni = 2002;
+const now = 2020;
+const job = 'student';
+
+// const moni_spl = "I'm " + name + ', a ' + (now-moni) + ' year old ' + job + '!';
+// console.log(moni_spl);
+
+// const moni_spl = `I'm ${name}, a ${now-moni} year old ${job}!`;
+// console.log(moni_spl);
+
+/*TEST DATA 1:*/
+// const year = 2020;
+// const bday = 1888;
+
+// const age1 = year - bday;
+
+// if(age1 < 18){
+//     console.log("You are NOT Eligible");
+// } 
+// else if(age1 > 65) {
+//     console.log("You are NOT Eligible");
+// }
+
+// else{
+//     console.log("You are Eligible");
+// }
+
+
+
+
